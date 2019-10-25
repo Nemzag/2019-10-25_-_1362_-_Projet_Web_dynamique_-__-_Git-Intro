8673b1a405a6ffdc69225ccf0d8c0f6b7a75a6fa
Arifi Gazmen <nemzag@homail.com>
2019-10-25 14:11:32 +0200

    Premier commit de structure de fichiers

---
+++ assets/js/jquery.js
@@ -0,0 +1,7 @@
+<!--
+ * Created by PhpStorm.
+ * User: Nemzag aka Gaz‑mên Arifi (Shkypi, 1979-09-30).
+ * Date: 25/10/2019
+ * Time: 14:06
+ * Updated:
+-->
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * Created by PhpStorm.
+ * User: Nemzag aka Gaz‑mên Arifi (Shkypi, 1979-09-30).
+ * Date: 25/10/2019
+ * Time: 14:01
+ * Updated:
+*/
+?>
+
+<pre><code class="language-git line-numbers">gid add readme.md
+git status</code></pre>
+
+		<pre><code class="language-git line-numbers">git restore readme.md
+git status</code></pre>
+
+				<pre><code class="language-git line-numbers">git reset readme.md
+git status</code></pre>
+
+Command grouper :
+<pre><code class="language-git line-numbers">git add --all</code>
+
+</code></pre>
